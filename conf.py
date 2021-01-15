13dd973d629bbf55867a13a0fa44bdfb1e41e763
Brian Clemens <bclemens@ctrliq.com>
2021-01-15 13:42:59 +0900

    Add logo, use RTD theme

--- conf.py
+++ conf.py
@@ -47,7 +47,46 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#html_theme_options = {}
+html_theme_options = {
+    'sticky_navigation': True,
+    'includehidden': True,
+    'navigation_depth': 5,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+}
+
+html_context = {
+    'display_github': True,
+    'github_user': 'ctrliq',
+    'github_repo': 'warewulf-docs',
+    'github_version': 'master',
+    'conf_py_path': '/',
+}
+
+# Add any paths that contain custom themes here, relative to this directory.
+#html_theme_path = []
+
+# The name for this set of Sphinx documents.  If None, it defaults to
+# "<project> v<release> documentation".
+#html_title = None
+
+# A shorter title for the navigation bar.  Default is the same as html_title.
+#html_short_title = None
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = 'logo.png'
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = 'favicon.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
